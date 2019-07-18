f685f4c49e2a2604b1fcbbf6eef72c864c21812c
GoSeungUks <51691326+GoSeungUks@users.noreply.github.com>
2019-07-19 00:23:30 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -973,7 +973,7 @@ async def on_message(msg):
 		if message.content.startswith('!v') or message.content.startswith('!ㅍ'):
 			tmp_sayMessage = message.content
 			sayMessage = tmp_sayMessage[3:]
-			await MakeSound(message.author.display_name +'님이.' + sayMessage, './sound/say')
+			await MakeSound(sayMessage, './sound/say')
 			await client.get_channel(channel).send("```< " + msg.author.display_name + " >님이 \"" + sayMessage + "\"```", tts=False)
 			await PlaySound(voice_client1, './sound/say.mp3')
 
