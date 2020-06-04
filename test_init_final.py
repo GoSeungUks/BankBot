326e040c85029f4a445591818db9957231b619e4
GoSeungUks <51691326+GoSeungUks@users.noreply.github.com>
2020-06-04 16:33:23 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -1562,7 +1562,7 @@ while True:
 				result = wks.acell(basicSetting[16]).value
 
 				embed = discord.Embed(
-						description= '```' + SearchID + ' 님이 받을 다이야는 ' + result + ' 다이야 입니다.```',
+						description= '```' + SearchID + ' 님이 받을 다이아는 ' + result + ' 다이아 입니다.\n정산 다이아는 판매 완료된 금액의 합계 입니다.\n물품 등록이 완료되면 R총무 로 쪽지 보내주세요.```',
 						color=0xff00ff
 						)
 				await ctx.send(embed=embed, tts=False)
